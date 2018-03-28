95178abffcf5f26577bd107c3584dc11eef59af5
Brandon <b.mazey@nyu.edu>
2018-03-28 10:24:47 -0400

    creating exercise modules!

---
+++ verify/src/VerifyInstall.java
@@ -0,0 +1,13 @@
+public class VerifyInstall {
+
+    public static void main (String [] args) {
+
+        /**
+         * Run this program to validate that your IDE is working.
+         * You should be able to see a message appear in the console below without any errors.
+         */
+
+        System.out.println("Looks great! Don't forget to send your github username to b.mazey@nyu.edu!");
+
+    }
+}
