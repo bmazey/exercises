06572696cbf5b9ba0908f1b3e9c32cfc086647d1
Brandon <b.mazey@nyu.edu>
2018-06-22 15:52:41 -0400

    adding in interview challenge ...

---
+++ interview/src/main/java/FizzBuzz.java
@@ -0,0 +1,18 @@
+public class FizzBuzz {
+
+    /**
+     * Welcome to the Interview Challenge!
+     * @param args
+     */
+
+    public static void main(String [] args) {
+
+    }
+
+    public String fizzBuzz(int i) {
+
+        //TODO - implement method!
+        return new String();
+
+    }
+}
