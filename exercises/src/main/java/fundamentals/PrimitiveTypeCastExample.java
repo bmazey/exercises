b6e5b0041f862cd888255b395fd389bd70223ec2
Brandon <b.mazey@nyu.edu>
2018-06-26 16:00:55 -0400

    ummm delted the primitive type cast example

--- exercises/src/main/java/fundamentals/PrimitiveTypeCastExample.java
+++ exercises/src/main/java/fundamentals/PrimitiveTypeCastExample.java
@@ -4,6 +4,32 @@ public class PrimitiveTypeCastExample {
 
     public static void main(String[] args) {
 
+        int i = 10;
+        double k = 2.5;
+        double j = 3.5;
+        int w = 2 * i;
+        boolean guess = false;
+        char c = '1';
+
+        // && "and"
+        // || "or"
+
+        if ((i > k) && (w > j)) {
+            System.out.println("true!");
+            System.out.println("second statement!");
+        }
+
+        if (!guess) {
+            System.out.println("guess equals " + guess);
+            System.out.println("who woudl've guessed?!");
+        }
+
+
+        else if (i < k) System.out.println("else if says true!");
+
+        else System.out.println("false!");
+
+        String fox = "the quick brown fox jumps over the lazy dog.";
 
     }
 
