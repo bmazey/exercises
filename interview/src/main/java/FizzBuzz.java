276cb7223eaaa198158b1f6c0fd4bf34c409e306
ninahutzler <mh3899@columbia.edu>
2018-06-27 10:24:42 -0400

    test commit

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -21,8 +21,6 @@ public class FizzBuzz {
             System.out.println( n ) ;
         }
 
-        //let's try to push
-
         int j = 101640 ;
 
         if ((j % 15) == 0 ) { // here I'm testing if the third number (int j) is divisible by 3 and 5
