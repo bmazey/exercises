4e520064b71ef10e2d9bd6019e136e83edc0862e
CLT <jc5096@columbia.edu>
2018-06-27 08:37:45 -0400

    password progress

--- interview/src/main/java/PasswordGenerator.java
+++ interview/src/main/java/PasswordGenerator.java
@@ -10,9 +10,19 @@ public class PasswordGenerator {
     }
 
     public static String generatePassword() {
-
-       //TODO - implement method!
-
-        return new String();
+String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOQRSTUVWXYZ0123456789";
+String password = "";
+Random randvalue = new Random();
+       int loop;
+       //adding letters
+       for(loop = 1; loop <= 4; loop++)
+           password += (characters.charAt(randvalue.nextInt(52)));
+       //adding numbers
+       for(loop = 1; loop <= 4; loop++)
+           password += (characters.charAt(randvalue.nextInt(10) + 52));
+       //adding rand letters-numbers
+       for(loop = 1; loop <= 2; loop++)
+           password += (characters.charAt(randvalue.nextInt(62)));
+        return password;
     }
 }
