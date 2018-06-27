acdd234618d690a0e1de39902ee0a65827ebf31e
Arjay48 <ryan.jing48@gmail.com>
2018-06-27 10:43:50 -0400

    Made the result string thing.

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -11,33 +11,37 @@ public class FizzBuzz {
 
     public String fizzBuzz(int i) {
 
+        String result = "";
 
+        /** Result is the string that gets put into the test.
+         */
 
         if (i%3==0)
             if (i%5==0)
-                System.out.print("fizzbuzz");
+                result += "fizzbuzz";
             else
-                System.out.print("fizz");
+                result += "fizz";
 
-        /** This part first tests if "i" is divisible by 3. Divisibility is tested by seeing if the remainder is 0. If it is,
-         * it then tests for divisibility by 5. If it is also divisible by 5, "fizzbuzz" is printed, and if it is only divisible
-         * by 3, "fizz" is printed.
+        /** This part tests if "i" is divisible by 3. Divisibility is tested by seeing if the remainder = 0. If it is,
+         * it then tests for divisibility by 5. If it is also divisible by 5, "fizzbuzz" is added onto "result", and if it is only divisible
+         * by 3, "fizz" is added to "result".
          */
 
         else if (i%5==0)
-            System.out.print("buzz");
+            result += "buzz";
 
-        /** If "i" is not divisible by 3, the program then tests for divisibility by 5. If it is divisible, then "buzz" printed.
+        /** If "i" is not divisible by 3, the program then tests for divisibility by 5. If it is divisible, then "buzz" is added onto "result".
          */
 
         else
-            System.out.print(i);
+            result += i;
 
         /** Finally, if "i" is divisible by neither 5 nor 3, the program simply prints the value of "i".
          */
 
 
-        return new String();
+
+        return result;
 
     }
 }
