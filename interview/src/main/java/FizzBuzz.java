5db9f7e4057312d92d3958dc9f57dc1067e1d00c
elliemeiers <em3398@columbia.edu>
2018-06-27 10:03:35 -0400

    hiding the answer

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -2,39 +2,21 @@ public class FizzBuzz {
 
     /**
      * Welcome to the Interview Challenge!
+     *
      * @param args
      */
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
-        int i = 231 ;
-        String m = "fizz" ;
 
-        if ((i % 3) == 0) { // here I'm testing if the first number (int i) is divisible by 3
-            System.out.println( m ) ;
-        }
-
-        int k = 440 ;
-        String n = "buzz" ;
-
-        if ((k % 5) == 0) { // here I'm testing if the second number (int k) is divisible by 5
-            System.out.println( n ) ;
-        }
-
-        //test comment 2
-
-        int j = 101640 ;
-
-        if ((j % 15) == 0 ) { // here I'm testing if the third number (int j) is divisible by 3 and 5
-            System.out.println( m + n ) ;
-        }
 
     }
 
-      //  public String fizzBuzz(int i) {
+    public String fizzBuzz(int i) {
 
-     //TODO - implement method!
-     //  return new String();
+        //TODO - implement method!
+        return new String();
 
-     }
+    }
 
+}
