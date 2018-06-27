39751a8adb4cf2e1a7edb5154f02243069be9bbc
ninahutzler <mh3899@columbia.edu>
2018-06-26 21:02:26 -0400

    Worked on the fizzbuzz

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -7,12 +7,32 @@ public class FizzBuzz {
 
     public static void main(String [] args) {
 
+        int i = 231 ;
+        String m = "fizz" ;
+
+        if ((i % 3) == 0) { // here I'm testing if the first number (int i) is divisible by 3
+            System.out.println( m ) ;
+        }
+
+        int k = 440 ;
+        String n = "buzz" ;
+
+        if ((k % 5) == 0) { // here I'' testing if the second number (int k) is divisible by 5
+            System.out.println( n ) ;
+        }
+
+        int j = 101640 ;
+
+        if ((j % 15) == 0 ) {
+            System.out.println( m + n ) ;
+        }
+
     }
 
-    public String fizzBuzz(int i) {
+      //  public String fizzBuzz(int i) {
 
-        //TODO - implement method!
-        return new String();
+     //TODO - implement method!
+     //  return new String();
 
     }
 }
