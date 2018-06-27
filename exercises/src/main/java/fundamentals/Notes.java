26ac5b3fc87e1db85b7270eed8b71fe1a62cf92f
ninahutzler <mh3899@columbia.edu>
2018-06-27 09:30:34 -0400

    test commit

---
+++ exercises/src/main/java/fundamentals/Notes.java
@@ -0,0 +1,28 @@
+package fundamentals;
+
+public class Notes {
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        double k = 2.5;
+        double j = 3.5;
+        int w = 2 * i;
+
+        if (((i < k)) && ((w > j))) System.out.println("false");
+
+        else System.out.println("true!");
+
+        String fox = "the quick brown fox jumps over the lazy dog";
+
+        int a = 231;
+        int b = 3;
+        int c = a % b;
+
+        if (c == 0) {
+            System.out.println("fizz");
+
+
+        }
+    }
+}
