cd15ecba25c5c9570c1dfacbacf4f6d303a24285
sa3647 <sa3647@columbia.edu>
2018-06-27 13:13:33 -0400

    first commit!

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -2,20 +2,27 @@ public class FizzBuzz {
 
     /**
      * Welcome to the Interview Challenge!
-     *
      * @param args
      */
 
-    public static void main(String[] args) {
-
-    //cleaned up development
-
+    public static void main(String [] args) {
+        FizzBuzz myFizzBuzz= new FizzBuzz();        //
+        int i =231;                                 // define variable
+        int j =440;
+        int k=101640;
+        myFizzBuzz.fizzBuzz(i);                     // Plugs in value into function
+        myFizzBuzz.fizzBuzz(j);
+        myFizzBuzz.fizzBuzz(k);
     }
-
-    public String fizzBuzz(int i) {
-
+    public String fizzBuzz(int i) {            // implementing the method
+        if (i%15 == 0)                              // checks if # is divisible by 3&5
+            System.out.println("FizzBuzz");         // if true prints "FizzBuzz" if not moves on
+        else if (i%3 == 0)                          // checks if # is divisible by 3
+            System.out.println("Fizz");             // if true prints "Fizz" if not moves on
+        else if (i%5 == 0)                          // checks if # is divisible by 5
+            System.out.println("Buzz");             // if true prints "Buzz" if not moves on
         //TODO - implement method!
-        return new String();
+        return new String();                       // since nothing is divisible by 3 or 5 prints nothing
 
     }
 
