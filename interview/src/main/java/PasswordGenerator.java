b3c17a62fd7ef2bf8f07c3e7e2a75835e78ec6cc
CLT <jc5096@columbia.edu>
2018-06-27 15:06:38 -0400

    finished fizzbuzz, palidome, and password generator.

--- interview/src/main/java/PasswordGenerator.java
+++ interview/src/main/java/PasswordGenerator.java
@@ -10,19 +10,18 @@ public class PasswordGenerator {
     }
 
     public static String generatePassword() {
-String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~`!@#$%^&*()_-+={[}]|\\:;\"\'<,>.?/";
 String password = "";
 Random randvalue = new Random();
        int loop;
        //adding letters
-       for(loop = 1; loop <= 4; loop++)
+       for(loop = 1; loop <= 5; loop++)
            password += (characters.charAt(randvalue.nextInt(52)));
        //adding numbers
        for(loop = 1; loop <= 4; loop++)
            password += (characters.charAt(randvalue.nextInt(10) + 52));
-       //adding rand letters-numbers
-       for(loop = 1; loop <= 2; loop++)
-           password += (characters.charAt(randvalue.nextInt(62)));
+       //adding rand letter-number
+           password += (characters.charAt(randvalue.nextInt(32) + 62));
        //debug
        System.out.println(password);
         return password;
