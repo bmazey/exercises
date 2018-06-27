cac8a47dddfdac3b1a3d76a0e06bd196764daecb
tkmourad123 <tm2997@columbia.edu>
2018-06-27 00:29:22 -0400

    Day 1 of FizzBuzz problem

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -11,6 +11,15 @@ public class FizzBuzz {
 
     public String fizzBuzz(int i) {
 
+        public String fizzBuzz(int i) {
+
+            if (i % 3 == 0)
+                return ("fizz");
+            else if (i % 5 == 0)
+                return ("buzz");
+            else if (i % 5 == 0 && i % 3 ==0)
+                return ("fizzbuzz");
+
         //TODO - implement method!
         return new String();
 
