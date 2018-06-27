4c97e963fa955258d40ac7204824c42aa16de958
Arjay48 <ryan.jing48@gmail.com>
2018-06-27 10:19:23 -0400

    Typed some

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -11,7 +11,32 @@ public class FizzBuzz {
 
     public String fizzBuzz(int i) {
 
-        //TODO - implement method!
+
+
+        if (i%3==0)
+            if (i%5==0)
+                System.out.print("fizzbuzz");
+            else
+                System.out.print("fizz");
+
+        /** This part first tests if "i" is divisible by 3. Divisibility is tested by seeing if the remainder is 0. If it is,
+         * it then tests for divisibility by 5. If it is also divisible by 5, "fizzbuzz" is printed, and if it is only divisible
+         * by 3, "fizz" is printed.
+         */
+
+        else if (i%5==0)
+            System.out.print("buzz");
+
+        /** If "i" is not divisible by 3, the program then tests for divisibility by 5. If it is divisible, then "buzz" printed.
+         */
+
+        else
+            System.out.print(i);
+
+        /** Finally, if "i" is divisible by neither 5 nor 3, the program simply prints the value of "i".
+         */
+
+
         return new String();
 
     }
