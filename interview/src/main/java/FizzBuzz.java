ebe4deb2cd5591b349b677b61d5aacfba1343ce4
Мечта <madi.lukmanov@gmail.com>
2018-06-27 10:45:57 -0400

    New changes.Don't forget about array.

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -6,14 +6,21 @@ public class FizzBuzz {
      */
 
     public static void main(String [] args) {
-        for(int i=1;i<=100; i++);
+        
     }
 
 
     public String fizzBuzz(int i) {
-
-        //TODO - implement method!
-        return new String();
+        String result="";
+        if  (i%3==0)
+    if (i%5==0)
+        result+="fizzbuzz";
+else
+    System.out.println("fizz");
+        //TODO - implement method;
+            else if (i%5==0)
+          result+="buzz";
+        return result;
 
     }
 }
