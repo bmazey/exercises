53afdf6c71f5f85f69b7875307fc163c83cba393
ninahutzler <mh3899@columbia.edu>
2018-06-27 10:57:15 -0400

    fizzbuzz

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -2,37 +2,45 @@ public class FizzBuzz {
 
     /**
      * Welcome to the Interview Challenge!
+     *
      * @param args
      */
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
-        int i = 231 ;
-        String m = "fizz" ;
+        int i = 231;
+        String m = "fizz";
+        // String result = "" ;
 
-        if ((i % 3) == 0) { // here I'm testing if the first number (int i) is divisible by 3
-            System.out.println( m ) ;
+        if ((i % 3) == 0) {
+            System.out.println(m);
         }
 
-        int k = 440 ;
-        String n = "buzz" ;
+        int k = 440;
+        String n = "buzz";
 
         if ((k % 5) == 0) { // here I'm testing if the second number (int k) is divisible by 5
-            System.out.println( n ) ;
+            System.out.println(n);
         }
 
-        int j = 101640 ;
+        int j = 101640;
 
-        if ((j % 15) == 0 ) { // here I'm testing if the third number (int j) is divisible by 3 and 5
-            System.out.println( m + n ) ;
+        if ((j % 15) == 0) { // here I'm testing if the third number (int j) is divisible by 3 and 5
+            System.out.println(m + n);
         }
 
     }
 
-      //  public String fizzBuzz(int i) {
+    public String fizzBuzz ( int i ){
+        String result = "";
 
-     //TODO - implement method!
-     //  return new String();
+        if (i % 3 == 0) result += "fizz";
 
-     }
+        if (i % 5 == 0) result += "buzz";
+
+        //TODO - implement method!
+        return result;
+
+    }
+}
 
