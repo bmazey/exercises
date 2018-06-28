3da284c8536d57eaea1bdc19cd67ca67fafa182d
abbepiels <ap3789@columbia.edu>
2018-06-28 16:29:00 -0400

    Commit

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -28,7 +28,16 @@ public class FizzBuzz {
 
     public String fizzBuzz(int i) {
         //TODO - implement method!
-        return new String();
+
+
+        if (i%15 == 0)
+            return ("fizzbuzz");
+        else if (i%3 == 0)
+           return ("fizz");
+        else
+            return ("buzz");
+
+
     }
 }
 
