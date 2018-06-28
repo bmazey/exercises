95af581fe36465fff5b034e2f1f57acb767b836a
Brandon <b.mazey@nyu.edu>
2018-06-27 22:26:09 -0400

    ok! looks good!

--- exercises/src/main/java/fundamentals/Dog.java
+++ exercises/src/main/java/fundamentals/Dog.java
@@ -45,6 +45,6 @@ public class Dog {
          * This is also a comment!
          */
         //System.out.println(spot.allDogsGoToHeaven());
-        //test comment
+        //test comment2
     }
 }
