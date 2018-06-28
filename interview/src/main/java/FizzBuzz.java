7575fe5b0daac466c4154f233eb1fbbefdf8136d
kelcieengles <kee2124@columbia.edu>
2018-06-27 21:17:31 -0400

    Finished Palendrome test

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -1,15 +1,30 @@
 public class FizzBuzz {
 
+
     /**
      * Welcome to the Interview Challenge!
+     *
      * @param args
      */
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
+
 
     }
 
-    public String fizzBuzz(int i) {
+    public String fizzBuzz(int s) {
+        if (s % 3 == 0 && s % 5 == 0) {
+            return ("fizzbuzz");
+        }
+
+        if (s % 3 == 0) {
+            return ("fizz");
+
+        }
+        if (s % 5 == 0) {
+            return ("buzz");
+        }
+
 
         //TODO - implement method!
         return new String();
--- interview/src/main/java/Palindrome.java
+++ interview/src/main/java/Palindrome.java
@@ -11,6 +11,13 @@ public class Palindrome {
 
     public boolean isPalindrome(String s) {
 
+    int n = (s.length());
+    for(int i = 0; i < (n/2); ++i) {
+        if (s.charAt(i) != s.charAt(n - i - 1)) {
+            return false;
+        }
+    }
+
         //TODO - implement method!
 
         return true;
