d604a716f54800c4be3f99595a6d7010e8830606
maddiezietsman <mz2712@columbia.edu>
2018-06-28 14:54:36 -0400

    working on palindrome

--- interview/src/main/java/Palindrome.java
+++ interview/src/main/java/Palindrome.java
@@ -11,9 +11,9 @@ public class Palindrome {
 
     public boolean isPalindrome(String s) {
 
-        int n = (s.length());
-        for (int i = 0; i<(n/2); ++i) {
-            if (s.charAt(i) !=s.charAt(n - i - 1)) {
+        int n = s.length();
+        for (int i = 0; i < (n/2); i++) {
+            if (s.charAt(i) = (s.charAt(n - i)) {
                 return false;
             }
         }
