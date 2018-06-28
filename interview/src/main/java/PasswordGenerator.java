a683d8d020bb1af0f833a06778e4c9b4f08f361a
paigemanning <pm2997@columbia.edu>
2018-06-28 17:26:53 -0400

    password generator!

--- interview/src/main/java/PasswordGenerator.java
+++ interview/src/main/java/PasswordGenerator.java
@@ -11,8 +11,22 @@ public class PasswordGenerator {
 
     public static String generatePassword() {
 
+        String password = "";
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        Random Num = new Random();
+        String symbol = "!@#$%^&*";
+
+        for (int i = 0; i < 5; i++) {
+            password += alphabet.charAt(Num.nextInt(25));
+        }
+
+        for (int i = 0; i < 4; i++) {
+            password += (Num.nextInt(9));
+        }
+
+        password += symbol.charAt(Num.nextInt(8));
        //TODO - implement method!
 
-        return new String();
+        return password;
     }
 }
