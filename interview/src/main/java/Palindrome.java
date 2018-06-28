da6fcb91279b89472b4b1d6c89d5e30400065613
Delmorear <yunfanzhang01@gmail.com>
2018-06-28 17:00:57 -0400

    FizzBuzzTest Passed. PalindromeTest Passed.

--- interview/src/main/java/Palindrome.java
+++ interview/src/main/java/Palindrome.java
@@ -1,22 +1,26 @@
 public class Palindrome {
 
-    /**
-     * Welcome to the Interview Challenge!
-     * @param args
-     */
+    public static void main(String [] args) {}
 
-    public static void main(String [] args) {
-
-    }
     public boolean isPalindrome(String s) {
-        //string for reversing word
-        String n = "";
-        //loop for copying word into its reverse
-            for(int var = s.length() - 1; var >= 0; var--)
-                {
-                    n += s.charAt(var);
-                }
-        //returning binary value
-        return (n.equals(s));
+
+        //Input string
+        boolean palindrome = true;
+        int checkAt = 0;
+
+        //Define the new variable "checkAt"
+        //Loop compare the following:
+        // 1. Whether the checked part fits the requirement of being a palindrome
+        // 2. Whether checking process reached the middle (0.5*stringLength)
+        while (palindrome && checkAt < s.length()){
+            palindrome = s.charAt(checkAt)==s.charAt(s.length()-1-checkAt);
+            checkAt ++;
+        }
+
+        //returning boolean
+        return (palindrome);
+
     }
 }
+
+// 06.28.18 Coding Fin ><(((º>
