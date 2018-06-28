848bcaf1c7c6b8886c1333f3982b98f7b2a55a8b
elliemeiers <em3398@columbia.edu>
2018-06-28 17:40:41 -0400

    done!!!

--- interview/src/main/java/PasswordGenerator.java
+++ interview/src/main/java/PasswordGenerator.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class PasswordGenerator {
 
-    public static void main (String [] args) {
+    public static void main(String[] args) {
 
         // for testing!
         System.out.println("password: " + generatePassword());
@@ -11,8 +11,25 @@ public class PasswordGenerator {
 
     public static String generatePassword() {
 
-       //TODO - implement method!
+        String result = "";
+        Random rand = new Random();
 
-        return new String();
+
+        String alphabet = "abcdefghijklmnopkrstuvwxyz";
+        String numbers = "0123456789";
+        String symbols = "!@#$%^&*";
+
+        for (int i = 0; i < 5; i++) {
+            result += alphabet.charAt(rand.nextInt(alphabet.length() - 1));
+        }
+
+        for (int i = 0; i < 4; i++) {
+            result += numbers.charAt(rand.nextInt(numbers.length() - 1));
+        }
+
+        result += symbols.charAt(rand.nextInt(symbols.length() - 1));
+
+        return result;
     }
 }
+
