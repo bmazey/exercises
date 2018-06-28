d12be007385f804ac9e0a1ec88a44e9b78b6f163
Delmorear <yunfanzhang01@gmail.com>
2018-06-28 16:29:54 -0400

    my first commit

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -1,26 +1,31 @@
 public class FizzBuzz {
 
-    /**
-     * Welcome to the Interview Challenge!
-     * @param args
-     */
-
-    public static void main(String [] args) {
-
+    public static void main(String[] args) {
     }
 
-    public String fizzBuzz(int i) {
+    public String fizzBuzz(int numberInput) {
 
-        if (i % 15 == 0) {
-            System.out.println("Fizzbuzz!");
+        // Test if the unknown input number is a multiple of 3 or 5 or 15
+        boolean multipleOfThree = numberInput % 3 == 0;
+        boolean multipleOfFive = numberInput % 5 == 0;
+        boolean multipleOfFifteen = numberInput % 15 == 0;
+
+        // Let the number go through the process of out put, 15 should be before 3 and 5
+        //15
+        if (multipleOfFifteen) {
+            System.out.print("fizzbuzz");
         }
-        else if (i % 3 == 0) {
-            System.out.println("Fizz");
+        //3
+        else if (multipleOfThree) {
+            System.out.print("fizz");
         }
-        else if (i % 5 == 0) {
-            System.out.println("Buzz");
+        //5
+        else if (multipleOfFive) {
+            System.out.print("buzz");
         }
+
         return new String();
-
     }
 }
+
+// 06.28.18 Coding Fin ><(((º>
