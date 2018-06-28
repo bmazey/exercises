e8c348561d86512feacaae1f1d9f79e26aaf90d9
Мечта <madi.lukmanov@gmail.com>
2018-06-28 16:15:02 -0400

    New changes.Don't forget about array.445454544545

--- interview/src/main/java/PasswordGenerator.java
+++ interview/src/main/java/PasswordGenerator.java
@@ -9,27 +9,54 @@ public class PasswordGenerator {
 
     }
 
-
     public static String generatePassword() {
 String password="";
+        String ik="abcdefghijklmnopqrstuvwxyz";
+        String po="0123456789";
+        String pl="!@#$%^&*()+=/";
+
         Random r = new Random();
-        char c = (char)(r.nextInt(26) + 'a');
-char a=(char)(r.nextInt(26)+'a');
-        char b = (char)(r.nextInt(26) + 'a');
-        char z = (char)(r.nextInt(26) + 'a');
-        char t = (char)(r.nextInt(26) + 'a');
-        char  n = (char)(r.nextInt(9) + '0');
-        char  y = (char)(r.nextInt(9) + '0');
-        char  o = (char)(r.nextInt(9) + '0');
-        char  p = (char)(r.nextInt(9) + '0');
+        int z=r.nextInt(25)+0;
+        char a=ik.charAt(z);
+        password+=a;
+        int q=r.nextInt(25)+0;
+        char b=ik.charAt(q);
+        password+=b;
+        int w=r.nextInt(25)+0;
+        char c=ik.charAt(w);
+        password+=c;
+        int o=r.nextInt(25)+0;
+        char p=ik.charAt(o);
+        password+=p;
+        int t=r.nextInt(25)+0;
+        char y=ik.charAt(t);
+        password+=y;
+        int f=r.nextInt(9)+0;
+        char g=po.charAt(f);
+        password+=g;
+        int i=r.nextInt(9)+0;
+        char l=po.charAt(i);
+        password+=l;
+        int u=r.nextInt(9)+0;
+        char n=po.charAt(u);
+        password+=n;
+        int m=r.nextInt(9)+0;
+        char k=po.charAt(m);
+        password+=k;
+
+        int h=r.nextInt(13)+0;
+        char x=pl.charAt(h);
+        password+=x;
 
 
 
-        for(int i=0;i<10;i++){
+
+
+        {
 
         }
 
 
-        return new String();
+        return password;
     }
 }
