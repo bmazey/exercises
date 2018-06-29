4d314853b2ad32be0cd9551443f572b5a785c6e7
Brenda Macias <brenda.macias98@gmail.com>
2018-06-29 15:14:53 -0400

    completed function

--- interview/src/main/java/PasswordGenerator.java
+++ interview/src/main/java/PasswordGenerator.java
@@ -10,9 +10,23 @@ public class PasswordGenerator {
     }
 
     public static String generatePassword() {
-
-       //TODO - implement method!
-
-        return new String();
+        String password = "";
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        String nums = "0123456789";
+        String chars = "@#$!";
+        Random rand = new Random();
+        for (int i = 0 ; i < 5; i++) {
+            int randint = rand.nextInt(25);
+            password += alpha.charAt(randint);
+        }
+        for (int i = 0; i < 4; i++){
+            int randint = rand.nextInt(9);
+            password += nums.charAt(randint);
+        }
+        for (int i = 0; i < 1; i++) {
+            int randint = rand.nextInt(3);
+            password += chars.charAt(3);
+        }
+        return password;
     }
 }
