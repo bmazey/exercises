1af8840be9a0f68cb6d9c9028a648038540976f3
elliemeiers <em3398@columbia.edu>
2018-06-29 17:06:22 -0400

    done!!!

--- interview/src/main/java/zoo/Panther.java
+++ interview/src/main/java/zoo/Panther.java
@@ -10,25 +10,46 @@ public class Panther extends Cat {
 
     //TODO - implement methods!
 
+    private Integer size = 5;
+    private Integer numberOfLegs = 4;
+    private String call = "Roar";
+    private Color color = Color.BLACK;
+    private String vertebrate = "true";
+
     void purr() { }
 
-    public Integer getSize() {
-        return null;
+    public Integer getSize() { return this.size; }
+
+    public void setSize (int size) {
+        if (size > 10) {
+            System.out.println("panther2 size: " +size + ". this panther is too BIG! it can't be" +size + ", it must be 10 or below!");
+            this.size = 10;
+        }
+
+
+        else {
+            this.size = size;
+        }
+
     }
+
 
     public Integer getNumberOfLegs() {
-        return null;
+        return this.numberOfLegs; }
+
+
+    public void setNumberOfLegs(int i) {
+        if (i > 10) {
+            System.out.println("Panther can't be bigger than 10");
+            this.size = 5;
+        } else {
+            this.size = i;
+        }
     }
 
-    public String getCall() {
-        return null;
-    }
+    public String getCall() { return this.call; }
 
-    public Color getColor() {
-        return null;
-    }
+    public Color getColor()  {return this.color;}
 
-    public Vertebrate getVertebrate() {
-        return null;
-    }
+    public Vertebrate getVertebrate() {return Vertebrate.MAMMAL; }
 }
