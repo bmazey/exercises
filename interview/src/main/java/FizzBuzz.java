8b2b92ac5e0a6be85b4872f7c390a3c398984278
Ahmad Badghaish <40617439+Ahmad-bad@users.noreply.github.com>
2018-06-29 12:47:18 -0400

    Update FizzBuzz.java

--- interview/src/main/java/FizzBuzz.java
+++ interview/src/main/java/FizzBuzz.java
@@ -10,17 +10,9 @@ public class FizzBuzz {
     }
 
     public String fizzBuzz(int i) {
-        String result = "";
-        if (i % 15 == 0) { //we have to check for the 15 first so that if it was a fizzbuzz it doesn't appear as a fizz!
-            result = "Fizzbuzz!";
-        }
-        else if (i % 3 == 0) { // The "%" sign is called the "Modulo" sign, which checks the remainder after division :D
-            result = "Fizz";
-        }
-        else if (i % 5 == 0) {
-            result = "Buzz";
-        }
-        return result;
+
+        //TODO - implement method!
+        return new String();
 
     }
 }
