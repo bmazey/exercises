f13c7008ebc3b891c0a2b45275d4748b229a0ccf
sa3647 <sa3647@columbia.edu>
2018-06-29 15:33:00 -0400

    completed interview

--- interview/src/main/java/PasswordGenerator.java
+++ interview/src/main/java/PasswordGenerator.java
@@ -10,9 +10,26 @@ public class PasswordGenerator {
     }
 
     public static String generatePassword() {
+        String password = "";
+        String passwordLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        String passwordNumbers = "0123456789";
+        String passwordSymbols = "~!@#$%^&*()_+-=`";
+    Random rand = new Random();
 
-       //TODO - implement method!
+        for (int i = 0; i < 5; i++) {
+            int randomIndex = rand.nextInt(passwordLetters.length());
+            password += (passwordLetters.charAt(randomIndex));
+        }
 
-        return new String();
+
+        for (int i = 0; i < 4; i++){
+            int randomIndex2 = rand.nextInt(passwordNumbers.length());
+            password += (passwordNumbers.charAt(randomIndex2));
+        }
+
+    int randomIndex3 = rand.nextInt(passwordSymbols.length());
+        password += (passwordSymbols.charAt(randomIndex3));
+        return  password;
+
     }
 }
--- interview/src/main/java/zoo/Panther.java
+++ interview/src/main/java/zoo/Panther.java
@@ -8,27 +8,38 @@ public class Panther extends Cat {
      * @return
      */
 
-    //TODO - implement methods!
+    private Integer size = 5;
+    private Integer numberOfLegs = 4;
+    private String call = "Roar";
+    private String color = "BLACK";
+    private String vertebrate = "true";
 
     void purr() { }
 
-    public Integer getSize() {
-        return null;
+    public Integer getSize() {return this.size;}
+
+    public void setSize(int i) {
+        if (i > 10) {
+            System.out.println("Panthers can't be bigger than 10!");
+            this.size = 5;
+        }
+        else {
+            this.size = i;
+        }
     }
 
     public Integer getNumberOfLegs() {
-        return null;
+        return this.numberOfLegs;
     }
 
-    public String getCall() {
-        return null;
+    public void setNumberOfLegs(int i){
+        this.numberOfLegs = i;
     }
 
-    public Color getColor() {
-        return null;
-    }
+    public String getCall() {return this.call;}
 
-    public Vertebrate getVertebrate() {
-        return null;
+    public String getColor() {return this.color;}
+
+    public String getVertebrate() {return this.vertebrate;
     }
 }
