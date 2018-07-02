8ef3669c086f3f6979791e362c76a05b5ade375e
Brenda Macias <brenda.macias98@gmail.com>
2018-07-02 15:47:39 -0400

    more edits

--- exercises/src/main/java/structures/TreasureMap.java
+++ exercises/src/main/java/structures/TreasureMap.java
@@ -5,6 +5,10 @@ import java.util.Map;
 
 public class TreasureMap {
     public static void main (String[] args) {
+        HashMap<String,String> TreasureMap = new HashMap();
+        TreasureMap.put("beach","sandy shores");
+        TreasureMap.put("coast","ocean reef");
+        System.out.println(TreasureMap.get("beach"));
 
     }
 
@@ -13,6 +17,9 @@ public class TreasureMap {
         //TODO - implement this method!
         HashMap<String,String> TreasureMap = new HashMap();
         TreasureMap.put("beach","sandy shores");
-        return new HashMap<String, String>();
+        TreasureMap.put("coast","ocean reef");
+        TreasureMap.put("volcano","hot lava");
+        TreasureMap.put("x", "marks the spot");
+        return TreasureMap;
     }
 }
