54e0246de8f295e54a2416c149a20ed863334cbb
Delmorear <yunfanzhang01@gmail.com>
2018-07-02 16:26:22 -0400

    TreasureMap Test Passed MovieQueue Test Passed

--- exercises/src/main/java/structures/MovieQueue.java
+++ exercises/src/main/java/structures/MovieQueue.java
@@ -11,8 +11,13 @@ public class MovieQueue {
 
     public Queue<String> createMovieQueue() {
 
-        //TODO - implement this method!
+        Queue<String> MovieQueue = new LinkedList();
+        //Adding strings into queue
+        MovieQueue.add("Leonardo");
+        MovieQueue.add("Donatello");
+        MovieQueue.add("Raphael");
+        MovieQueue.add("Michelangelo");
 
-        return new LinkedList<String>();
+        return MovieQueue;
     }
 }
