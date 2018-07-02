687b008a415e9efe03aa8eb9bf445126eb1b42df
sa3647 <sa3647@columbia.edu>
2018-07-02 16:22:35 -0400

    treasure map

--- exercises/src/main/java/structures/TreasureMap.java
+++ exercises/src/main/java/structures/TreasureMap.java
@@ -9,9 +9,16 @@ public class TreasureMap {
     }
 
     public Map<String, String> createTreasureMap() {
+        HashMap <String, String> TreasureMap = new HashMap<String, String>();
+        TreasureMap.put("beach", "sandy shores");
+        TreasureMap.put("coast", "ocean reef");
+        TreasureMap.put("volcano", "hot lava");
+        TreasureMap.put("x", "marks the spot");
+
+
 
         //TODO - implement this method!
 
-        return new HashMap<String, String>();
+        return TreasureMap;
     }
 }
