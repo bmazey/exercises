3d5dc7e488298ae9ec423c14df115850e314f71b
ahmad <ahmad.badghaish1@gmail.com>
2018-07-02 11:21:36 -0400

    Ahmad's branch

--- interview/src/main/java/PasswordGenerator.java
+++ interview/src/main/java/PasswordGenerator.java
@@ -11,32 +11,27 @@ public class PasswordGenerator {
 
     public static String generatePassword() {
 
-        Random r = new Random();
+
 
         String password = "";
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
         String numbers = "0123456789";
         String symbols = "!@#$%^&*";
-        String[] allChars;
-        for (int a = 0;a < alphabet.length();a++) {
-            allChars[a] = String.valueOf();
-        }
-        for (int n = alphabet.length();n < (alphabet.length() + numbers.length());n++) {
-            int nn = 0;
-            allChars[n] = numbers.charAt(nn);
-            nn++;
-        }
-        for (int s = (alphabet.length()+numbers.length());s < (alphabet.length()+numbers.length()+symbols.length());s++)  {
-            int ss = 0;
-            allChars[s] = symbols.charAt(ss);
-            ss++;
-        }
-
-        int Low = 0;
-        int High = 100;
-        int Result = r.nextInt(High-Low) + Low;
+        String everything = alphabet+numbers+symbols;
+        String[] allChars = everything.split("");
 
 
-        System.out.println(allChars.length);
+        for (int o = 0;o < 10;o++) {
+            Random r = new Random();
+            int Low = 0;
+            int High = 45;
+            int Result = r.nextInt(High-Low) + Low;
+            password += allChars[Result];
         }
+
+
+
+
+        return password;
+    }
 }
