6aa0a8d0783cf4639cdc5cfce2ec1ca7f5e83205
Delmorear <yunfanzhang01@gmail.com>
2018-07-03 16:15:38 -0400

    PlagiarismTest Passed

--- exercises/src/main/java/algorithms/RabinKarp.java
+++ exercises/src/main/java/algorithms/RabinKarp.java
@@ -12,30 +12,30 @@ public class RabinKarp {
         q -> A prime number
     */
     public static ArrayList<Integer> search(String pat, String txt, int q) {
-        int M = pat.length();
-        int N = txt.length();
+        int patternLength = pat.length();
+        int textLength = txt.length();
         int i, j;
         int p = 0; // hash value for pattern
         int t = 0; // hash value for txt
         int h = 1;
 
         // list of matches to be returned
-        ArrayList<Integer> matches = new ArrayList<Integer>();
+        ArrayList<Integer> MatchingList = new ArrayList();
 
         // The value of h would be "pow(d, M-1)%q"
-        for (i = 0; i < M-1; i++)
+        for (i = 0; i < patternLength-1; i++)
             h = (h*d)%q;
 
         // Calculate the hash value of pattern and first
         // window of text
-        for (i = 0; i < M; i++)
+        for (i = 0; i < patternLength; i++)
         {
             p = (d*p + pat.charAt(i))%q;
             t = (d*t + txt.charAt(i))%q;
         }
 
         // Slide the pattern over text one by one
-        for (i = 0; i <= N - M; i++)
+        for (i = 0; i <= textLength - patternLength; i++)
         {
 
             // Check the hash values of current window of text
@@ -43,9 +43,21 @@ public class RabinKarp {
             // check for characters on by one
 
             //TODO - implement this method! :)
+
+            /* For current index i, check for pattern
+              match */
+            for (j = 0; j < patternLength; j++)
+                if (txt.charAt(i + j) != pat.charAt(j))
+                    break;
+
+            if (j == patternLength) { // if pat[0...M-1] = txt[i, i+1, ...i+M-1]
+                System.out.println("Pattern found at index " + i);
+                MatchingList.add(i);
+            }
+
         }
 
-        return matches;
+        return MatchingList;
     }
 
     /* Driver program to test above function */
@@ -57,3 +69,5 @@ public class RabinKarp {
         search(pat, txt, q);
     }
 }
+
+//07.03.18 Cheating Fin ><(((('>
