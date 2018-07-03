0a0cd98003cc74f852e4e268ad3af799119fc568
sa3647 <sa3647@columbia.edu>
2018-07-02 22:41:19 -0400

    Treasure Map + Grocery List

--- exercises/src/main/java/structures/GroceryList.java
+++ exercises/src/main/java/structures/GroceryList.java
@@ -1,17 +1,28 @@
 package structures;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GroceryList {
     public static void main (String[] args) {
 
     }
 
-    public List<String> createGroceryList() {
+
+    public List<String>  createGroceryList(){
+
+        List<String> GroceryList = new ArrayList<String>();
+        GroceryList.add(0,"milk");
+        GroceryList.add(1, "eggs");
+        GroceryList.add(2,"seltzer");
+        GroceryList.add(3,"cheese");
+        GroceryList.add(4,"seltzer");
+        GroceryList.size();
 
         //TODO - implement this method!
 
-        return new ArrayList<String>();
+         return GroceryList;
     }
 }
--- exercises/src/main/java/structures/MovieQueue.java
+++ exercises/src/main/java/structures/MovieQueue.java
@@ -10,7 +10,14 @@ public class MovieQueue {
     }
 
     public Queue<String> createMovieQueue() {
+        MovieQueue myMovieQueue = new MovieQueue();
+        Queue<String> myQueue = myMovieQueue.createMovieQueue();
+        myQueue.add("Leonardo");
+        myQueue.add("Donatello");
+        myQueue.add("Raphael");
+        myQueue.add("Michelangelo");
 
+        myQueue.size();
         //TODO - implement this method!
 
         return new LinkedList<String>();
