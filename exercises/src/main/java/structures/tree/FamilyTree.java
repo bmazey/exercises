078a82c1d51ab3873736ab914bed30ec0a9c3354
maddiezietsman <mz2712@columbia.edu>
2018-07-03 16:07:44 -0400

    finished family tree

--- exercises/src/main/java/structures/tree/FamilyTree.java
+++ exercises/src/main/java/structures/tree/FamilyTree.java
@@ -8,8 +8,19 @@ public class FamilyTree {
 
     public TreeNode<String> createFamilyTree() {
 
+        TreeNode<String> FamilyTree = new TreeNode<String> ("Lucille");
+        FamilyTree.addChild("George Oscar");
+        TreeNode<String> child2 = FamilyTree.addChild("Michael");
+        TreeNode<String> child3 = FamilyTree.addChild("Lindsay");
+        FamilyTree.addChild("Buster");
+
+        child2.addChild("George Michael");
+        child3.addChild("Maeby");
+
+
+
         //TODO - implement this method!
 
-        return new TreeNode<String>("root");
+        return FamilyTree;
     }
 }
