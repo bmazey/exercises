f4bc6631c25b2b8341c33546b230e7ed89674a4e
sa3647 <sa3647@columbia.edu>
2018-07-03 10:41:29 -0400

    Treasure Map + Grocery List + Movie Queue + Family Tree

--- exercises/src/main/java/structures/tree/FamilyTree.java
+++ exercises/src/main/java/structures/tree/FamilyTree.java
@@ -7,9 +7,17 @@ public class FamilyTree {
     }
 
     public TreeNode<String> createFamilyTree() {
+        TreeNode<String> rootNode = new TreeNode<String>("Lucille");
 
+        TreeNode<String> child1 = rootNode.addChild("George Oscar");
+        TreeNode<String> child2 = rootNode.addChild("Michael");
+        TreeNode<String> child3 = rootNode.addChild("Lindsay");
+        TreeNode<String> child4 = rootNode.addChild("Buster");
+
+        TreeNode<String> grandchild1 = child2.addChild("George Michael");
+        TreeNode<String> grandchild2 = child3.addChild("Maeby");
         //TODO - implement this method!
 
-        return new TreeNode<String>("root");
+        return rootNode;
     }
 }
