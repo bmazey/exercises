f50b5d32313afbdf793198e393ab6cd6c6cbd17f
ninahutzler <mh3899@columbia.edu>
2018-07-05 16:44:21 -0400

    Edited Hieroglyph

--- exercises/src/main/java/expressions/Hieroglyph.java
+++ exercises/src/main/java/expressions/Hieroglyph.java
@@ -7,45 +7,57 @@ public class Hieroglyph {
 
     public boolean worshipSacredCats(String s) {
 
-        //TODO - implement this method!
+        Pattern p = Pattern.compile("(.*)cat(.*)");
+        Matcher m = p.matcher(s);
 
-        return true;
+        boolean b = m.matches();
+
+
+        return b;
     }
 
     public boolean discoverPyramidInSand(String s) {
 
-        //TODO - implement this method!
+        Pattern p = Pattern.compile("sand pyramid sand");
+        Matcher m = p.matcher(s);
+        boolean b = m.matches();
 
-        return true;
+        return b;
     }
 
     public boolean readAlphanumericGlyph(String s) {
 
-        //TODO - implement this method!
+        Pattern p = Pattern.compile("[0-9]{2,4}[a-z]{3,4}");
+        Matcher m = p.matcher(s);
+        boolean b = m.matches();
 
-        return true;
+        return b;
     }
 
     public boolean captureGoldScarab(String s) {
 
-        //TODO - implement this method!
+        Pattern p = Pattern.compile("(.*)gold scarab");
+        Matcher m = p.matcher(s);
 
-        return true;
+        return m.matches() ;
     }
 
     public boolean avoidNileCrocodile(String s) {
 
-        //TODO - implement this method!
+        Pattern p = Pattern.compile("(?!.*crocodile).*nile.*(?!.*crocodile)");
+        // "(?!.*crocodile).*nile.*(?!.*crocodile)"
+        // "nile( nile)*"
+        Matcher m = p.matcher(s);
 
-        return true;
+        return m.matches();
     }
 
 
     public String replaceWithFalseIdol(String s) {
 
-        //TODO - implement this method!
+        String replaceString = s.replaceAll("skull", "idol" );
 
-        return new String();
+        return replaceString;
     }
 
     public static void main (String[] args) {
