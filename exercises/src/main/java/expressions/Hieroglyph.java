06eef25abaca0f91a8b6eb03f4f1d7b3b6639a32
zuritamckinnon <az2537@columbia.edu>
2018-07-05 16:58:00 -0400

    finished hieroglyph test

--- exercises/src/main/java/expressions/Hieroglyph.java
+++ exercises/src/main/java/expressions/Hieroglyph.java
@@ -7,51 +7,48 @@ public class Hieroglyph {
 
     public boolean worshipSacredCats(String s) {
 
-        //TODO - implement this method!
-
-        return true;
+       Pattern c = Pattern.compile(".*cat.*");
+       Matcher cats = c.matcher(s);
+       return cats.matches();
     }
 
     public boolean discoverPyramidInSand(String s) {
 
-        //TODO - implement this method!
-
-        return true;
+        Pattern p = Pattern.compile("sand pyramid sand");
+        Matcher pyramids = p.matcher(s);
+        boolean morepyramids = pyramids.matches();
+        return morepyramids;
     }
 
     public boolean readAlphanumericGlyph(String s) {
 
-        //TODO - implement this method!
-
-        return true;
+        Pattern p = Pattern.compile("\\d{2,4}\\w{3,4}");
+        Matcher password = p.matcher(s);
+        boolean morepasswords = password.matches();
+        return morepasswords;
     }
 
     public boolean captureGoldScarab(String s) {
 
-        //TODO - implement this method!
-
-        return true;
+        Pattern g = Pattern.compile(".*gold scarab");
+        Matcher gold = g.matcher(s);
+        boolean moregold = gold.matches();
+        return moregold;
     }
 
     public boolean avoidNileCrocodile(String s) {
 
-        //TODO - implement this method!
-
-        return true;
+        Pattern n = Pattern.compile("(?!.*crocodile).*nile.*");
+            //Pattern n = Pattern.compile("nile ( nile)*");
+        Matcher nile = n.matcher(s);
+        boolean morenile = nile.matches();
+        return morenile;
     }
 
 
     public String replaceWithFalseIdol(String s) {
 
-        //TODO - implement this method!
-
-        return new String();
-    }
-
-    public static void main (String[] args) {
-
-        /**
-         * EXPERIMENTS ONLY!
-         */
+        String f = s.replaceAll("skull", "idol");
+        return f;
     }
 }
