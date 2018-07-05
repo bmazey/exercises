1d9bf035f03b4625b2e1eb93eb9a1847c3eb6518
maddiezietsman <mz2712@columbia.edu>
2018-07-05 16:29:25 -0400

    finished alphanumeric glyph

--- exercises/src/main/java/expressions/Hieroglyph.java
+++ exercises/src/main/java/expressions/Hieroglyph.java
@@ -7,23 +7,37 @@ public class Hieroglyph {
 
     public boolean worshipSacredCats(String s) {
 
+        Pattern cat = Pattern.compile(".*cat.*");
+        Matcher c = cat.matcher(s);
+        boolean bCat = c.matches();
+
+
         //TODO - implement this method!
 
-        return true;
+        return bCat;
     }
 
     public boolean discoverPyramidInSand(String s) {
 
+
+        Pattern sand = Pattern.compile("sand.pyramid.sand");
+        Matcher a = sand.matcher(s);
+        boolean aSand = a.matches();
+
         //TODO - implement this method!
 
-        return true;
+        return aSand;
     }
 
     public boolean readAlphanumericGlyph(String s) {
 
+        Pattern AlphanumericGlyph = Pattern.compile("[1-9]{2,4}[a-z]{3,4}");
+        Matcher n = AlphanumericGlyph.matcher(s);
+        boolean nAlphanumericGlyph = n.matches();
+
         //TODO - implement this method!
 
-        return true;
+        return nAlphanumericGlyph;
     }
 
     public boolean captureGoldScarab(String s) {
