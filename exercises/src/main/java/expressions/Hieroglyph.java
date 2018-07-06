e4c97039ba016058b31f3cdd9ab918b1211f2b28
ahmad <ahmad.badghaish1@gmail.com>
2018-07-06 12:32:44 -0400

    All Done!!

--- exercises/src/main/java/expressions/Hieroglyph.java
+++ exercises/src/main/java/expressions/Hieroglyph.java
@@ -6,28 +6,28 @@ import java.util.regex.Pattern;
 public class Hieroglyph {
 
     public boolean worshipSacredCats(String s) {
-        Pattern p = Pattern.compile("(.*)cat(.*)");
+        Pattern p = Pattern.compile("(.*)cat(.*)");  // It works!
         Matcher m = p.matcher(s);
         boolean b = m.matches();
         return b;
     }
 
     public boolean discoverPyramidInSand(String s) {
-        Pattern p = Pattern.compile("sand pyramid sand");
+        Pattern p = Pattern.compile("sand pyramid sand"); // Works!
         Matcher m = p.matcher(s);
         boolean b = m.matches();
         return b;
     }
 
     public boolean readAlphanumericGlyph(String s) {
-        Pattern p = Pattern.compile("[1-9]{2-4}[a-z][3-4]");
+        Pattern p = Pattern.compile("[1-9]{2-4}[a-z][3-4]");   // Not working ;(
         Matcher m = p.matcher(s);
         boolean b = m.matches();
         return b;
     }
 
     public boolean captureGoldScarab(String s) {
-        Pattern p = Pattern.compile(".*(scarab gold scarab|gold scarab).*");
+        Pattern p = Pattern.compile("(.*)gold scarab(.*)");  // Perfectly working!
         Matcher m = p.matcher(s);
         boolean b = m.matches();
         return b;
