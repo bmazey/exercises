efa4825be624647f8340a6031d50eb46e0710c71
elliemeiers <em3398@columbia.edu>
2018-07-09 16:04:54 -0400

    DONE!!

--- exercises/src/main/java/patterns/builder/CarBuildDirector.java
+++ exercises/src/main/java/patterns/builder/CarBuildDirector.java
@@ -13,30 +13,57 @@ public class CarBuildDirector {
 
     public Car constructSUV() {
 
+        Car mySuv = new Car();
+        mySuv.setColor("Yellow");
+        mySuv.setDoors(4);
+        mySuv.setWheels(4);
+        mySuv.setHasTrunk(true);
+        mySuv.setFuel("Unleaded Gas");
         //TODO - implement this method! :)
 
-       return new Car();
+       return mySuv;
     }
 
     public Car constructSportscar() {
 
+        Car mySportsCar = new Car();
+        mySportsCar.setColor("Red");
+        mySportsCar.setDoors(2);
+        mySportsCar.setWheels(4);
+        mySportsCar.setHasTrunk(false);
+        mySportsCar.setFuel("Premium Gas");
+
         //TODO - implement this method! :)
 
-        return new Car();
+        return mySportsCar;
     }
 
     public Car constructEighteenWheeler() {
 
+        Car myEighteenWheeler = new Car();
+        myEighteenWheeler.setColor("Blue");
+        myEighteenWheeler.setDoors(2);
+        myEighteenWheeler.setWheels(18);
+        myEighteenWheeler.setHasTrunk(false);
+        myEighteenWheeler.setFuel("Diesel");
+
         //TODO - implement this method! :)
 
-        return new Car();
+        return myEighteenWheeler;
     }
 
     public Car constructTesla() {
 
+        Car myTesla = new Car();
+        myTesla.setColor("Black");
+        myTesla.setDoors(4);
+        myTesla.setWheels(4);
+        myTesla.setHasTrunk(true);
+        myTesla.setFuel("Electricity");
+
         //TODO - implement this method! :)
 
-        return new Car();
+        return myTesla;
     }
 
 }
