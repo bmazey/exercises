7e8a1e4cd669b3465d97dd61c4ba8edf44fe9859
leohsu2906 <lh2937@gmail.com>
2018-07-09 15:59:24 -0400

    ok

--- exercises/src/main/java/patterns/builder/CarBuildDirector.java
+++ exercises/src/main/java/patterns/builder/CarBuildDirector.java
@@ -5,6 +5,7 @@ public class CarBuildDirector {
      * This is just an example of how to use a "director" class to use the builder pattern.
      *
      */
+
     private CarBuilder builder;
 
     public CarBuildDirector(final CarBuilder builder) {
@@ -15,28 +16,49 @@ public class CarBuildDirector {
 
         //TODO - implement this method! :)
 
-       return new Car();
+
+        return builder.setWheels(4)
+                .setColor("Yellow")
+                .setHasTrunk(true)
+                .setFuel("Unleaded Gas")
+                .setDoors(4)
+                .build();
     }
 
     public Car constructSportscar() {
 
         //TODO - implement this method! :)
 
-        return new Car();
+        return builder.setWheels(4)
+                .setColor("Red")
+                .setHasTrunk(false)
+                .setFuel("Premium Gas")
+                .setDoors(2)
+                .build();
     }
 
     public Car constructEighteenWheeler() {
 
         //TODO - implement this method! :)
 
-        return new Car();
+        return builder.setWheels(18)
+                .setColor("Blue")
+                .setHasTrunk(false)
+                .setFuel("Diesel")
+                .setDoors(2)
+                .build();
     }
 
     public Car constructTesla() {
 
         //TODO - implement this method! :)
 
-        return new Car();
+        return builder.setWheels(4)
+                .setColor("Black")
+                .setHasTrunk(true)
+                .setFuel("Electricity")
+                .setDoors(4)
+                .build();
     }
 
 }
