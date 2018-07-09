bdc32a423e054c130486d2ea8067b3ccb8593988
sa3647 <sa3647@columbia.edu>
2018-07-09 16:07:49 -0400

    Car Builder

--- exercises/src/main/java/patterns/builder/CarBuildDirector.java
+++ exercises/src/main/java/patterns/builder/CarBuildDirector.java
@@ -12,31 +12,72 @@ public class CarBuildDirector {
     }
 
     public Car constructSUV() {
+        builder.setHasTrunk(true);
+        builder.setFuel("Unleaded Gas");
+        builder.setDoors(4);
+        builder.setColor("Yellow");
+        builder.setWheels(4);
 
         //TODO - implement this method! :)
 
-       return new Car();
+       return builder.setWheels(4)
+        .setColor("Yellow")
+        .setDoors(4)
+        .setFuel("Unleaded Gas")
+        .setHasTrunk(true)
+        .build();
     }
 
     public Car constructSportscar() {
+        builder.setHasTrunk(false);
+        builder.setFuel("Premium Gas");
+        builder.setDoors(2);
+        builder.setColor("Red");
+        builder.setWheels(4);
 
         //TODO - implement this method! :)
 
-        return new Car();
+        return builder.setHasTrunk(false)
+        .setFuel("Premium Gas")
+        .setDoors(2)
+        .setColor("Red")
+        .setWheels(4)
+        .build();
     }
 
     public Car constructEighteenWheeler() {
+        builder.setHasTrunk(false);
+        builder.setFuel("Diesel");
+        builder.setDoors(2);
+        builder.setColor("Blue");
+        builder.setWheels(18);
 
         //TODO - implement this method! :)
 
-        return new Car();
+        return builder.setHasTrunk(false)
+                .setFuel("Diesel")
+                .setDoors(2)
+                .setColor("Blue")
+                .setWheels(18)
+                .build();
     }
 
     public Car constructTesla() {
+        builder.setHasTrunk(true);
+        builder.setFuel("Electricity");
+        builder.setDoors(4);
+        builder.setColor("Black");
+        builder.setWheels(4);
 
         //TODO - implement this method! :)
 
-        return new Car();
+        return builder.setHasTrunk(true)
+                .setFuel("Electricity")
+                .setDoors(4)
+                .setColor("Black")
+                .setWheels(4)
+                .build();
     }
+
 
 }
