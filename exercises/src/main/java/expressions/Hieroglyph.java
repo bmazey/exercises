897cec1da91e66e74860e1c19a666302b5ebc364
sa3647 <sa3647@columbia.edu>
2018-07-09 10:54:03 -0400

    Hieroglyph

--- exercises/src/main/java/expressions/Hieroglyph.java
+++ exercises/src/main/java/expressions/Hieroglyph.java
@@ -6,65 +6,71 @@ import java.util.regex.Pattern;
 public class Hieroglyph {
 
     public boolean worshipSacredCats(String s) {
+
         Pattern p = Pattern.compile("(sacred )*cat( sacred)*");
         Matcher m = p.matcher(s);
         boolean b = m.matches ();
-        boolean b1 = Pattern.matches("(sacred )*cat( sacred)*", s);
+
+        //TODO - implement this method!
+
         return b;
     }
 
     public boolean discoverPyramidInSand(String s) {
-        Pattern p = Pattern.compile("(sand) pyramid (sand)");
+
+        Pattern p = Pattern.compile("(sand )+pyramid( sand)+");
         Matcher m = p.matcher(s);
-        boolean b = m.matches();
-        boolean b1 = Pattern.matches("(sand) pyramid (sand)", s);
-        return b;
+        boolean b = m.matches ();
+
 
         //TODO - implement this method!
 
+        return b;
     }
 
     public boolean readAlphanumericGlyph(String s) {
+
         Pattern p = Pattern.compile("[0-9]{2,4}[a-z]{3,4}");
         Matcher m = p.matcher(s);
         boolean b = m.matches();
-        boolean b1 = Pattern.matches("[0-9]{2,4}[a-z]{3,4}",s);
-        return b;
+
 
         //TODO - implement this method!
 
-
+        return b;
     }
 
     public boolean captureGoldScarab(String s) {
-        Pattern p = Pattern.compile("(scarab )?? gold scarab");
+
+        Pattern p = Pattern.compile("(scarab )*gold( scarab)+");
         Matcher m = p.matcher(s);
         boolean b = m.matches();
-        boolean b1 = Pattern.matches("(scarab )?? gold scarab",s);
-        return b;
 
         //TODO - implement this method!
 
+        return b;
     }
 
     public boolean avoidNileCrocodile(String s) {
-        Pattern p = Pattern.compile("(nile)* nile (nile)*");
+
+        Pattern p = Pattern.compile("(nile )*nile( nile)*");
         Matcher m = p.matcher(s);
         boolean b = m.matches();
-        boolean b1 = Pattern.matches("(nile)* nile (nile)*",s);
-        return b;
-
 
         //TODO - implement this method!
 
+        return b;
     }
 
 
     public String replaceWithFalseIdol(String s) {
 
+        String j = s.replaceAll("skull", "idol");
+
+
         //TODO - implement this method!
 
-        return new String();
+        return j;
     }
 
     public static void main (String[] args) {
@@ -72,5 +78,4 @@ public class Hieroglyph {
         /**
          * EXPERIMENTS ONLY!
          */
-    }
-}
+    }}
