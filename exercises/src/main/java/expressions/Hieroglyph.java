3faf1c8c241e3c9e38d4fcc51a682cdeb40b7112
sa3647 <sa3647@columbia.edu>
2018-07-12 11:26:21 -0400

    Car Builder + MAze Game

--- exercises/src/main/java/expressions/Hieroglyph.java
+++ exercises/src/main/java/expressions/Hieroglyph.java
@@ -37,4 +37,4 @@ public class Hieroglyph {
          * EXPERIMENTS ONLY!
          */
     }
-} 
+}
--- exercises/src/main/java/patterns/strategy/SimpleStrategy.java
+++ exercises/src/main/java/patterns/strategy/SimpleStrategy.java
@@ -1,5 +1,12 @@
 package patterns.strategy;
 
-public class SimpleStrategy {
-    // TODO - implement strategy
+public class SimpleStrategy implements StrategyInterface {
+
+    public String workString(String s1, String s2) {
+        if (s1.equals("Marina")){
+            String s3 = s2.replaceAll(s2, "Let's go!");
+        }
+        else {String s3 =  s2.replaceAll(s2, "Nah, you cannot go"); }
+        return s2;
+    }
 }
